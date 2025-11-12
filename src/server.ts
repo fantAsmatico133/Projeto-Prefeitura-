d3fe400d8decb0ea2ddefd6fe0d53d39007d84c6
salazarr0 <samuelguimaraes091@gmail.com>
2025-11-11 21:14:55 -0300

    commit minimo com praticamente nada de alteração

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { app } from "./app";
 import { denunciaRouter } from "./routes/denunciasRouter";
 import { usuarioRouter } from "./routes/usuarioRouter";
 import { departamentoRouter } from "./routes/departamentoRouter";
+import { tipoDenunciaRouter } from "./routes/tipoDenunciaRouter";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 app.use("/denuncias", denunciaRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/departamentos", departamentoRouter);
+app.use("/tipo-denuncia", tipoDenunciaRouter)
 
 const PORT = process.env.PORT || 3003;
 
